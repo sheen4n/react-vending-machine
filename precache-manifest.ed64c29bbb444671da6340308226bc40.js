366dca49310b5a1bec1f3a2e0a557481348ed0e5
Goh Sheen An <sheen.an.goh@gmail.com>
2020-03-22 19:03:52 +0800

    Updates

---
+++ precache-manifest.ed64c29bbb444671da6340308226bc40.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3597729baefad0d5a730",
+    "url": "/react-vending-machine/static/css/main.b34b5a19.chunk.css"
+  },
+  {
+    "revision": "3597729baefad0d5a730",
+    "url": "/react-vending-machine/static/js/main.3597729b.chunk.js"
+  },
+  {
+    "revision": "f66d9077b521a2aae182",
+    "url": "/react-vending-machine/static/js/runtime~main.f66d9077.js"
+  },
+  {
+    "revision": "d76232b34c5010497d2c",
+    "url": "/react-vending-machine/static/js/2.d76232b3.chunk.js"
+  },
+  {
+    "revision": "7746d7d5d94a644ffd6cdb1876f72a87",
+    "url": "/react-vending-machine/static/media/Soda.7746d7d5.png"
+  },
+  {
+    "revision": "0e21446e438c1aeb2913a4c95776436a",
+    "url": "/react-vending-machine/static/media/Chips.0e21446e.png"
+  },
+  {
+    "revision": "369bea35cf50c57aa4cb31df6c8f4da3",
+    "url": "/react-vending-machine/static/media/VendingMachine.369bea35.png"
+  },
+  {
+    "revision": "d69eff61d12505692733a628037c183f",
+    "url": "/react-vending-machine/index.html"
+  }
+];
